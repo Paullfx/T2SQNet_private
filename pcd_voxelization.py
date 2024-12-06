2b8dd8150e6fbd1282640d9aeb33bcbaba3c4b16
fuxiao_lsy <paul.fx1999@gmail.com>
2024-12-06 11:07:28 +0100

    Begin to work on "fitting on segmented pcd"

--- pcd_voxelization.py
+++ pcd_voxelization.py
@@ -1,23 +1,9 @@
 import open3d as o3d
 import os
-import torch
-from models.pipelines import TSQPipeline
-import yaml
-
 
 
 # load ply file of cup from /home/fuxiao/Projects/Orbbec/concept-graphs/conceptgraph/dataset/external/tableware_4_6/exps/exp_default/tableware_4_6_bowl_denoised.ply
 tableware_ply_path = "/home/fuxiao/Projects/Orbbec/concept-graphs/conceptgraph/dataset/external/tableware_4_6/exps/exp_default/tableware_4_6_bowl_denoised.ply"
 if not os.path.exists(tableware_ply_path):
     raise FileNotFoundError(f"File not found: {tableware_ply_path}")
-
-
-def main():
-
-#obj_info = self.param_predictors[object_idx](voxel.unsqueeze(0), voxel_scale).squeeze().
-
-
-
-if __name__ == "__main__":
-    main()
 
