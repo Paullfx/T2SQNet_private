09db9a290d65bca08599ed783d90344a2de2f1b8
fuxiao_lsy <paul.fx1999@gmail.com>
2024-12-20 18:23:07 +0100

    save

--- pcd_voxelization_mesh.py
+++ pcd_voxelization_mesh.py
@@ -6,32 +6,43 @@ import matplotlib.pyplot as plt
 
 # reference: https://www.open3d.org/docs/latest/tutorial/Advanced/surface_reconstruction.html, focus on "Poisson surface reconstruction"
 # important intuition of parameter "depth", higher depth, more detailed mesh
+# need to firsty generate normals for the mesh
 
 
+# load and visualize the segmented pcd from CG pipleine
 
-tableware_ply_path = "./data_CG/tableware_4_16_bowl_denoised.ply" # a cup
+#tableware_ply_path = "./data_CG/tableware_4_16_bowl_denoised.ply" # a cup
+tableware_ply_path = "/home/fuxiao/Projects/Orbbec/concept-graphs/conceptgraph/dataset/external/tableware_5_12/exps/exp_default/tableware_5_12_bowl_denoised.ply"
+
 point_cloud = o3d.io.read_point_cloud(tableware_ply_path)
-
-
 print(point_cloud)
-o3d.visualization.draw_geometries([point_cloud],
-                                  zoom=0.664,
-                                  front=[-0.4761, -0.4698, -0.7434],
-                                  lookat=[1.8900, 3.2596, 0.9284],
-                                  up=[0.2304, -0.8825, 0.4101])
+#o3d.visualization.draw_geometries([point_cloud])
 
+
+
+# normal estimation
+point_cloud.normals = o3d.utility.Vector3dVector(np.zeros((1, 3)))
+point_cloud.estimate_normals()
+#o3d.visualization.draw_geometries([point_cloud], point_show_normal=True)
+point_cloud.orient_normals_consistent_tangent_plane(100) # It is observed that the normal reorientation leads to a worse (even more incomplete) mesh representation
+#o3d.visualization.draw_geometries([point_cloud], point_show_normal=True)
+
+
+
+
+# print('run Poisson surface reconstruction')
+# with o3d.utility.VerbosityContextManager(
+#         o3d.utility.VerbosityLevel.Debug) as cm:
+#     mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+#         point_cloud, depth=9) # depth can be adjustable here
+# print(mesh)
 
 print('run Poisson surface reconstruction')
-with o3d.utility.VerbosityContextManager(
-        o3d.utility.VerbosityLevel.Debug) as cm:
-    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
-        point_cloud, depth=9) # depth can be adjustable here
+mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+    point_cloud, depth=9)  # depth can be adjustable here
 print(mesh)
-o3d.visualization.draw_geometries([mesh],
-                                  zoom=0.664,
-                                  front=[-0.4761, -0.4698, -0.7434],
-                                  lookat=[1.8900, 3.2596, 0.9284],
-                                  up=[0.2304, -0.8825, 0.4101])
+
+# o3d.visualization.draw_geometries([mesh])
 
 
 # use pseudo-color to visualize the density
@@ -45,20 +56,30 @@ density_mesh.vertices = mesh.vertices
 density_mesh.triangles = mesh.triangles
 density_mesh.triangle_normals = mesh.triangle_normals
 density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
-o3d.visualization.draw_geometries([density_mesh],
-                                  zoom=0.664,
-                                  front=[-0.4761, -0.4698, -0.7434],
-                                  lookat=[1.8900, 3.2596, 0.9284],
-                                  up=[0.2304, -0.8825, 0.4101])
+o3d.visualization.draw_geometries([density_mesh])
 
 
 # remove low density vertices
 print('remove low density vertices')
-vertices_to_remove = densities < np.quantile(densities, 0.01) #output bool
+vertices_to_remove = densities < np.quantile(densities, 0.09) #set the density threshold #0.15 or even higher to crop the low density mesh of beer bottle
 mesh.remove_vertices_by_mask(vertices_to_remove) # o3d.geometry.TriangleMesh.remove_vertices_by_mask requires boolean values with the number of vertices
 print(mesh)
-o3d.visualization.draw_geometries([mesh],
-                                  zoom=0.664,
-                                  front=[-0.4761, -0.4698, -0.7434],
-                                  lookat=[1.8900, 3.2596, 0.9284],
-                                  up=[0.2304, -0.8825, 0.4101])
+
+# Recalculate the densities for the remaining vertices
+# Note: Open3D does not maintain per-vertex densities after filtering; you'll need to map the original densities back.
+filtered_densities = densities[~vertices_to_remove]  # Only keep densities for remaining vertices
+filtered_density_colors = plt.get_cmap('plasma')(
+    (filtered_densities - filtered_densities.min()) / (filtered_densities.max() - filtered_densities.min())
+)[:, :3]  # Map densities to colors
+
+# Apply pseudo-color based on the new density values
+filtered_density_mesh = o3d.geometry.TriangleMesh()
+filtered_density_mesh.vertices = mesh.vertices
+filtered_density_mesh.triangles = mesh.triangles
+filtered_density_mesh.triangle_normals = mesh.triangle_normals
+filtered_density_mesh.vertex_colors = o3d.utility.Vector3dVector(filtered_density_colors)
+
+# Visualize the filtered mesh with pseudo-color
+o3d.visualization.draw_geometries([filtered_density_mesh])
+
+#o3d.visualization.draw_geometries([mesh])
